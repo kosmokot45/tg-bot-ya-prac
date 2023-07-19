88ca9d85c8e4fa099aefdb4912e8bba4dbe35f03
pohofmd <kosmokot45@gmail.com>
2023-07-20 02:14:14 +0300

    add yandex speech kit API

--- bot.py
+++ bot.py
@@ -1,27 +1,91 @@
-import telebot
-from settings import BOT_TOKEN
+from telebot import TeleBot, types
+import requests
+import time
+import json
+from settings import BOT_TOKEN, REP_LINK, YA_IAM_TOKEN
 
-bot = telebot.TeleBot(BOT_TOKEN)
+bot = TeleBot(BOT_TOKEN)
+iam_key = YA_IAM_TOKEN
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_message(message):
-    bot.reply_to(message, "Hello Eugene Popov (lox)!")
+    bot.reply_to(message, f"Привет {message.chat.first_name}!")
 
-@bot.message_handler(commands=['watch1', 'watch2'])
+@bot.message_handler(content_types=["voice"])
+def voice_rec(message):
+    file_info = bot.get_file(message.voice.file_id)
+    file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(BOT_TOKEN, file_info.file_path))
+    
+    POST = "https://transcribe.api.cloud.yandex.net/speech/stt/v2/longRunningRecognize"
+    body ={
+        "config": {
+            "specification": {
+                "languageCode": "ru-RU"
+            }
+        },
+        "audio": {
+            "uri": file
+        }
+    }
+
+    header = {'Authorization': 'Bearer {}'.format(iam_key)}
+    
+    req = requests.post(POST, headers=header, json=body)
+    data = req.json()
+    print(data)
+    id = data['id']
+    while True:
+
+        time.sleep(1)
+
+        GET = "https://operation.api.cloud.yandex.net/operations/{id}"
+        req = requests.get(GET.format(id=id), headers=header)
+        req = req.json()
+
+        if req['done']: break
+        print("Not ready")
+    print("Text chunks:")
+    for chunk in req['response']['chunks']:
+        print(chunk['alternatives'][0]['text'])
+    bot.reply_to(req['response']['chunks'])
+
+@bot.message_handler(commands=['cat', 'dog'])
 def send_watch(message):
-    if message[0] == 'watch1':
-        bot.send_photo('img/cat.jpg')
-    elif message[0] == 'watch2':
-        bot.send_photo('img/dog.jpg')
-    
-    
-
+    if message.text.lower() == '/cat':
+        with open('img/cat.jpg', 'rb') as file:
+            bot.send_photo(message.chat.id, file)
+    elif message.text.lower() == '/dog':
+        with open('img/dog.jpg', 'rb') as file:
+            bot.send_photo(message.chat.id, file)
+      
 @bot.message_handler(commands=['read'])
 def send_text(message):
     pass
 
 @bot.message_handler(commands=['voice'])
-def send_text(message):
+def send_voice(message):
     pass
+
+@bot.message_handler(commands=['site'])
+def send_site(message):
+    bot.reply_to(message, REP_LINK)
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(
+        types.InlineKeyboardButton(
+            'Связаться с автором', url='telegram.me/pohuelofmind'
+        )
+    )
+    bot.send_message(message.chat.id, 
+                 'Для получения ссылки на репозиторий - /site \n'+
+                 'Хотите котю? - /cat \n'+
+                 'Хотите доги? - /dog', reply_markup=keyboard
+                 )
+
+@bot.message_handler()
+def send_unknow(message):
+    bot.reply_to(message, 'Я еще не знаю такой команды :(')
 
 bot.infinity_polling()
--- settings.py
+++ settings.py
@@ -4,3 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+REP_LINK = os.getenv('REP_LINK')
+YA_API_ID = os.getenv('YA_API_ID')
+YA_API_KEY = os.getenv('YA_API_KEY')
+YA_IAM_TOKEN = os.getenv('YA_IAM_TOKEN')
